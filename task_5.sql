770644d0248df32f573d5a8be2d82a42b93e45b2
yennulomlarri <yennulomlarri@gmail.com>
2025-08-17 11:05:35 +0000

    Final code correction for all tasks

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,3 @@
-USE alx_book_store;
-
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- Use INSERT IGNORE to prevent an error if the record already exists.
+INSERT IGNORE INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
